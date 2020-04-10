3a967ca10c7f002bf38358ce9d08d884caf2edb5
Aminata13 <59060784+Aminata13@users.noreply.github.com>
2020-04-10 15:01:25 +0000

    US1: Integration de la page de connexion

--- quizz/login.php
+++ quizz/login.php
@@ -11,29 +11,35 @@ require_once('../tp4-tableaux/functions.php');
 </head>
 <body>
     <?php
-        $username = "Mina";
-        $password = "MinaMina";
+        $username = "";
+        $password = "";
     ?>
     <header class="header">
+        <div class="logo"><img src="images/logo-QuizzSA.png" alt=""></div>
         <div class="title">Le Plaisir de Jouer</div>
-        <div class="logo"></div>
     </header>
-    <div class="content"> blabla
-        <!-- <div class="form-login">lalala
+    <div class="content">
+        <div class="form-login">
             <form class="form" method="POST">
-                <div class="form-content">
-                    <div class="form-icon icon1"><img src="../icone-user.png" alt=""></div>
-                    <div class="form-input"><input type="text" placeholder="Utilisateur" name="size" value="<?php echo $username;?>"></div>
+                <div class="form-header">
+                    <div class="form-header-text">Login Form</div>
                 </div>
                 <div class="form-content">
-                    <div class="form-icon"><img src="../icone-password.png" alt=""></div>
-                    <div class="form-input"><input type="text" placeholder="Mot de passe" name="size" value="<?php echo $password;?>"></div> 
+                    <div class="form-input"><input type="text" placeholder="Login" name="size" value="<?php echo $username;?>"></div>
+                    <div class="form-icon icon1"><img src="images/icônes/ic-login.png" alt=""></div>
+                </div>
+                <div class="form-content">
+                    <div class="form-input"><input type="text" placeholder="Password" name="size" value="<?php echo $password;?>"></div> 
+                    <div class="form-icon"><img src="images/icônes/ic-password.png" alt=""></div>
                 </div>
                 <div class="submit">
-                    <div class="button"><input type="submit" name="login" value="Connexion"></div>
+                    <div class="button button1"><input type="submit" name="login" value="Connexion"></div>
+                </div>
+                <div class="submit">
+                    <div class="button button2"><input type="submit" name="signup" value="S'inscrire pour jouer?"></div>
                 </div>
             </form>
-        </div> -->
+        </div>
     </div>
 </body>
 </html>
